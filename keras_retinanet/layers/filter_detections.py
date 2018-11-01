6ae56e33089f972d7a5c81a755fa1f2c7b23eab7
Mehul Sampat <mpsampat@gmail.com>
2018-11-01 00:00:22 +0000

    add  parameters for stage2 exp

--- keras_retinanet/layers/filter_detections.py
+++ keras_retinanet/layers/filter_detections.py
@@ -26,7 +26,7 @@ def filter_detections(
     nms                   = True,
     score_threshold       = 0.05,
     max_detections        = 300,
-    nms_threshold         = 0.1
+    nms_threshold         = 0.5
 ):
     """ Filter detections using the boxes and classification values.
 
